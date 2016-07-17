18fac7a1f237c9ac9890731b5bd7ddff9615b52e
Josh Hawkins <jkhaccounts@gmail.com>
2016-07-17 00:33:43 -0500

    Add camelCase, Title Case, Start case, GitHub link, refresh on change
    Also refactored content.js and added string prototyping.

--- js/content.js
+++ js/content.js
@@ -1,3 +1,54 @@
+// Add string methods
+String.prototype.toCamelCase = function() {
+    return this
+        .replace(/\s(.)/g, function($1) { return $1.toUpperCase(); })
+        .replace(/\s/g, '')
+        .replace(/^(.)/, function($1) { return $1.toLowerCase(); });
+}
+String.prototype.toStartCase = function() {
+    for (var i = 0; i < this.length; i++) {0
+        if (this.charAt(i).match(/[a-z]/i)) {
+            var char = this.charAt(i).toUpperCase();
+            return (this.slice(0, i) + char + this.slice(i + 1, this.length).toLowerCase());
+        }
+    }
+    return this;
+}
+String.prototype.toTitleCase = function(){
+    var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|vs?\.?|via)$/i;
+
+    return this.replace(/[A-Za-z0-9\u00C0-\u00FF]+[^\s-]*/g, function (match, index, title) {
+        if (index > 0 && index + match.length !== title.length &&
+          match.search(smallWords) > -1 && title.charAt(index - 2) !== ":" &&
+          (title.charAt(index + match.length) !== '-' || title.charAt(index - 1) === '-') &&
+          title.charAt(index - 1).search(/[^\s-]/) < 0) {
+            return match.toLowerCase();
+        }
+
+        if (match.substr(1).search(/[A-Z]|\../) > -1) {
+            return match;
+        }
+
+        return match.charAt(0).toUpperCase() + match.substr(1);
+    });
+};
+
+// Function to get desired case
+function getDesiredCase (str) {
+    if (!titleCase)
+        return;
+
+    if (titleCase === 'lower')
+        return str.toLowerCase();
+    if (titleCase === 'title')
+        return str.toTitleCase();
+    if (titleCase === 'start')
+        return str.toStartCase();
+    if (titleCase === 'camel')
+        return str.toCamelCase();
+    if (titleCase === 'all')
+        return str.toUpperCase();
+}
 
 // Load case
 var titleCase = '';
@@ -15,29 +66,19 @@ chrome.storage.sync.get('case', function(item) {
     try {
         var el = document.querySelector('.watch-title');
         var title = el.innerHTML;
-        if (titleCase === 'lower')
-            el.innerHTML = title.toLowerCase();
-        if (titleCase === 'all')
-            el.innerHTML = title.toUpperCase();
+        el.innerHTML = getDesiredCase(title);
     } catch (ex) {
         console.log('Encountered error, are we not on a main video page? ;', ex);
     }
-
     // Replace other titles (sidebar, etc)
     try {
         var titles = document.querySelectorAll('span.title');
         for (var i = 0; i < titles.length; i++) {
-            if (titleCase === 'lower')
-                titles[i].innerHTML = titles[i].innerHTML.toLowerCase();
-            if (titleCase === 'all')
-                titles[i].innerHTML = titles[i].innerHTML.toUpperCase();
+            titles[i].innerHTML = getDesiredCase(titles[i].innerHTML);
         }
         titles = document.querySelectorAll('a[title]');
         for (var i = 0; i < titles.length; i++) {
-            if (titleCase === 'lower')
-                titles[i].innerHTML = titles[i].innerHTML.toLowerCase();
-            if (titleCase === 'all')
-                titles[i].innerHTML = titles[i].innerHTML.toUpperCase();
+            titles[i].innerHTML = getDesiredCase(titles[i].innerHTML);
         }
     } catch (ex) {
         console.error(ex);
--- js/popup.js
+++ js/popup.js
@@ -15,6 +15,7 @@ angular.module('App', ['ngMaterial'])
                 else
                     App.case = 'lower';
 
+                // Refresh Angular
                 try {
                     $scope.$apply();
                 } catch (ex) {
@@ -22,6 +23,40 @@ angular.module('App', ['ngMaterial'])
                 }
 
                 console.log('Case loaded');
+            });
+        }
+
+        App.visit = function () {
+            // Anchor tag does not work as expected in action
+            chrome.tabs.create({ url: 'https://github.com/hawkins/quiet-title' });
+        }
+
+        App.refresh = function () {
+            chrome.tabs.query({active: true, currentWindow: true}, function (arrayOfTabs) {
+                chrome.tabs.executeScript(arrayOfTabs[0].id,
+                    // if (we're on YouTube.com)
+                    //     if (we're watching a video)
+                    //         refresh page but keep video time
+                    //     else
+                    //         just refresh if we're on YouTube somewhere else.
+                    {code: "if (window.location.href.includes('www.youtube.com/')) {\
+                                try {\
+                                    var time = document.getElementsByClassName('ytp-time-current')[0].innerHTML.split(':');\
+                                    var urlArgs = window.location.href.split('watch?v=')[1];\
+                                    var id = urlArgs.split('&')[0];\
+                                    var url = 'https://youtu.be/' + id + '?t=';\
+                                    if (time.length > 2) {\
+                                        url += time[0] + 'h';\
+                                        url.splice(0, 1);\
+                                    }\
+                                    if (time[0] != '0') url += time[0] + 'm';\
+                                    url += time[1] + 's';\
+                                    console.log(url);\
+                                    window.location.replace(url);\
+                                } catch (ex) {\
+                                    window.location.reload();\
+                                }\
+                            }"});
             });
         }
 
